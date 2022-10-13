f6a7b726ab8efcece34ae7bcb4100ad39aeef16f
jake <palatable7@naver.com>
2022-10-13 22:02:56 +0900

    Initial Commit

---
+++ ExImagePaint/ExImagePaintApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExImagePaintApp.swift
+//  ExImagePaint
+//
+//  Created by 김종권 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct ExImagePaintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
